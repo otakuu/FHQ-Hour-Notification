9c9ed338d7aaa829432a129ae676be81b105ba65
Dominic Allemann <dominic.allemann@compass-security.com>
2017-12-12 09:50:19 +0100

    new with background and without popup.js

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+chrome.browserAction.onClicked.addListener(function(tab) { 
+	
+	chrome.browserAction.getBadgeText({}, function(badgeText) {
+	
+		if(badgeText.length<4){	
+			//no badge is set - ENABLE
+			setAlarm();
+			
+		}else{
+			// badge is set - DIABLE
+			clearAlarm();
+		}
+
+	});
+	
+
+});
+
+
 chrome.alarms.onAlarm.addListener(function() {
 	
   var d = new Date();
@@ -89,4 +108,62 @@ function formatDate(date) {
   var year = date.getFullYear();
 
   return day + '. ' + monthNames[monthIndex] + ' ' + year;
+}
+
+
+
+function setAlarm() {
+	
+  var MINUTES = 15; 
+
+  //get time to desired alarm
+  var d = new Date();
+  var hrs = d.getHours();
+  var mins = d.getMinutes();
+  
+  var firstScheduled = (Math.floor((mins + MINUTES) / MINUTES)) * MINUTES;
+  
+  if(firstScheduled==60){
+	firstScheduled=0;  
+  }
+  
+  if(firstScheduled==0){
+	d.setHours(hrs+1);  
+  }
+  
+  d.setMinutes(firstScheduled);
+  d.setSeconds(0);  //very important
+  
+  //clear old ones
+  chrome.alarms.clearAll();
+  
+  //set new
+  chrome.alarms.clearAll();
+  chrome.alarms.create({when: d.getTime()});
+  
+  //get previous
+  var lastQuater = firstScheduled - MINUTES;
+  if(lastQuater < 0){
+	  lastQuater=45;
+	  d.setHours(hrs);  
+  }
+  d.setMinutes(lastQuater);
+  
+  chrome.browserAction.setBadgeText({text: getTime(d)});
+  //chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+  
+}
+
+function clearAlarm() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.alarms.clearAll();
+}
+
+
+function getTime(date){
+	
+	var mins = '0'+date.getMinutes(); //with nice preceding zero, if needed
+	var hrs = '0'+date.getHours();
+
+	return hrs.slice(-2) + mins.slice(-2);
 }
